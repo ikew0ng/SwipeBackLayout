08a1554d3647d901fece48f95fc5128013cd2d17
Issac <issac@imid.me>
2013-12-16 23:54:31 +0800

    format code

--- library/src/main/java/me/imid/swipebacklayout/lib/app/SwipeBackActivity.java
+++ library/src/main/java/me/imid/swipebacklayout/lib/app/SwipeBackActivity.java
@@ -1,12 +1,13 @@
 
 package me.imid.swipebacklayout.lib.app;
 
-import me.imid.swipebacklayout.lib.SwipeBackLayout;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
-public class SwipeBackActivity extends FragmentActivity implements SwipeBackActivityBase{
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+
+public class SwipeBackActivity extends FragmentActivity implements SwipeBackActivityBase {
     private SwipeBackActivityHelper mHelper;
 
     @Override
@@ -29,11 +30,12 @@ public class SwipeBackActivity extends FragmentActivity implements SwipeBackActi
             return mHelper.findViewById(id);
         return v;
     }
-    
+
     @Override
     public SwipeBackLayout getSwipeBackLayout() {
         return mHelper.getSwipeBackLayout();
     }
+
     @Override
     public void setSwipeBackEnable(boolean enable) {
         getSwipeBackLayout().setEnableGesture(enable);
